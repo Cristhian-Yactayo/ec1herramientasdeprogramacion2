ecf6384163512c11c1010e27e146e0b2d66205fd
Cristhian-Yactayo <yactayo5c@gmail.com>
2022-10-09 01:49:23 -0500

    Version 1.1

--- back/api-empresa/api-empresa/Controllers/EmpresaController.cs
+++ back/api-empresa/api-empresa/Controllers/EmpresaController.cs
@@ -1,6 +1,43 @@
-﻿namespace api_empresa.Controllers
+﻿using api_empresa.Entitys;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_empresa.Controllers
 {
-    public class EmpresaController
+    //decimos que es un controlador
+    [ApiController]
+
+    //es definir la ruta de acceso al controlador
+    [Route("api-empresa/empresa")]
+
+    //controlador base es una herencia para que sea controlador
+    public class EmpresaController: ControllerBase
     {
+        private readonly ApplicationDBContext context;
+
+        //creamos el metodo constructor
+        public EmpresaController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        //cuando queremos obtener informacion
+        [HttpGet]
+        public async Task<ActionResult<List<Empresa>>> findAll()
+        {
+            return await context.Empresa.ToListAsync();
+        }
+        //cuando queremos guardar informacion
+        [HttpPost]
+        public async Task<ActionResult> add(Empresa e)
+        {
+            context.Add(e);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
---
+++ back/api-empresa/api-empresa/Migrations/20221009051705_Inicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_empresa.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    dni = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    correo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.codigo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empresa");
+        }
+    }
+}
